7598d35a468a0094846ccc3057736fdd1aa81ed7
NedicMarina <marinanedic93@gmail.com>
2026-06-02 13:15:15 +0200

    Initial Web API CRUD project

---
+++ Store.WebApi/Controllers/AnimalController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.WebApi.Models;
+
+
+namespace Store.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimalsController : ControllerBase
+    {
+        private static List<Animal> animals = new List<Animal>
+        {
+            new Animal
+            {
+                Id = 1,
+                Species = "Lion",
+                Name = "Simba",
+                Age = 5,
+                FoodType = "Meat"
+            },
+            new Animal
+            {
+                Id = 2,
+                Species = "Elephant",
+                Name = "Dumbo",
+                Age = 10,
+                FoodType = "Grass"
+            }
+        };
+
+        [HttpGet(Name = "GetAnimal")]
+        public IEnumerable<Animal> GetAnimals()
+        {
+
+
+            return animals;
+        }
+
+        [HttpGet("{id}")]
+        public Animal GetAnimalById(int id)
+        {
+            Animal animal = animals.FirstOrDefault(animal => animal.Id == id);
+
+            return animal;
+        }
+
+
+        [HttpPost]
+        public Animal CreateAnimal(Animal animal)
+        {
+            int newId = animals.Max(animal => animal.Id) + 1;
+
+            animal.Id = newId;
+
+            animals.Add(animal);
+
+            return animal;
+        }
+
+        [HttpPut("{id}")]
+        public Animal UpdateAnimal(int id, Animal updatedAnimal)
+        {
+            Animal animal = animals.FirstOrDefault(animal => animal.Id == id);
+
+            if (animal == null)
+            {
+                return null;
+            }
+
+            animal.Species = updatedAnimal.Species;
+            animal.Name = updatedAnimal.Name;
+            animal.Age = updatedAnimal.Age;
+            animal.FoodType = updatedAnimal.FoodType;
+
+            return animal;
+        }
+
+        [HttpDelete("{id}")]
+        public Animal DeleteAnimal(int id)
+        {
+            Animal animal = animals.FirstOrDefault(animal => animal.Id == id);
+
+            if (animal == null)
+            {
+                return null;
+            }
+
+            animals.Remove(animal);
+
+            return animal;
+        }
+    }
+}
---
+++ Store.WebApi/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.WebApi.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Species { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string FoodType { get; set; }
+    }
+}
